6e6c9ca03926a7190ac58e41a95ff02bfd32c5d5
MilkoDimoski <milkomanchester@gmail.com>
2024-09-19 00:09:36 +0200

    first1 commit

---
+++ LMS_NVD-master/nov/src/views/MyCourses.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>My Courses</h2>
+    <ul>
+      <li v-for="course in myCourses" :key="course.id">
+<!--        ovde se enrolled courses -->
+        <!-- Link to the CourseDetail page using the course ID -->
+        <router-link :to="{ name: 'CourseDetails', params: { id: course.id }}">{{ course.title }}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+const myCourses = computed(() => {
+  // Assuming `currentUser` has enrolled courses
+  const currentUser = store.getters.currentUser;
+  console.log(currentUser);
+  return store.getters.getTeacherCoursesByUserId(currentUser.id) ;
+});
+</script>
+<style scoped>
+/* Add your styles here */
+</style>
